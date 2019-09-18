73d9d0e16ccadb799ef0e8273e969eb11eaf7fb3
Prince Ajuobi <princekelsey299@gmail.com>
2019-09-18 21:14:49 +0300

    Updates

---
+++ precache-manifest.0d4dbc1ace91d4d4c1e6a09057d17865.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9e7387e76fe2dc60c2b0e2ae13c9a6e",
+    "url": "/foodselector/index.html"
+  },
+  {
+    "revision": "7b520b5f925deb221eae",
+    "url": "/foodselector/static/css/main.0ecd6322.chunk.css"
+  },
+  {
+    "revision": "c5d155bab693a8f10134",
+    "url": "/foodselector/static/js/2.7769bd35.chunk.js"
+  },
+  {
+    "revision": "7b520b5f925deb221eae",
+    "url": "/foodselector/static/js/main.e69b07e1.chunk.js"
+  },
+  {
+    "revision": "63e8531ac9028bbb2fbc",
+    "url": "/foodselector/static/js/runtime~main.2f8fef39.js"
+  },
+  {
+    "revision": "adcf2b79feea97d1242ce6376c290057",
+    "url": "/foodselector/static/media/cook-366875_1920.adcf2b79.jpg"
+  }
+]);
